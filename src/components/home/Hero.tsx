fe10b1bd8f99334b677b523b09d3d280664c2739
Koome-eric <erickomee4195@gmail.com>
2025-11-27 09:53:02 +0300

    adding sitemap

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -19,7 +19,7 @@ export function Hero() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="/videos/safari2.mp4" type="video/mp4" />
+      <source src="/videos/safari2.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
